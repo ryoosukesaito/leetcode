c98c0480132d1de3a0cde49df5200ad9890e97f5
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-02-11 00:43:12 -0800

    solved Maximum Product of Three Numbers

---
+++ 628.max-product-of-three-numbers.js
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var maximumProduct = function (nums) {
+  nums.sort((a, b) => b - a);
+  const positiveSum = nums[0] * nums[1] * nums[2];
+  const negativeTwo = nums[0] * nums[nums.length - 1] * nums[nums.length - 2];
+
+  if (positiveSum > negativeTwo) {
+    return positiveSum;
+  } else {
+    return negativeTwo;
+  }
+};
