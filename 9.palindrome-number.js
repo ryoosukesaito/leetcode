93204cdf286def47212231fc1bed17f57229d285
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-02-15 22:18:03 -0800

    solved Palindrome Number

---
+++ 9.palindrome-number.js
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode.com/problems/palindrome-number/description/
+ */
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  const reversedX = x.toString().split("").reverse().join("");
+  return x===parseInt(reversedX) ? true : false;
+};
