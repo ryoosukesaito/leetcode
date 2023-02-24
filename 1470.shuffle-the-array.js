e8e3a7024bbaf407c6780830584a4cdfd20ef5b0
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-02-23 20:48:04 -0800

    solved Shuffle the Array

---
+++ 1470.shuffle-the-array.js
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/shuffle-the-array/description/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(nums[i], nums[i + n]);
+  }
+  return result;
+};
