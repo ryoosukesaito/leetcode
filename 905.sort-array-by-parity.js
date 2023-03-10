2e790ca93cd6c2ea894b78241db96ac1d0f2cd0a
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-03-09 22:46:37 -0800

    modified method using 2 filters without for loop

--- 905.sort-array-by-parity.js
+++ 905.sort-array-by-parity.js
@@ -7,14 +7,8 @@
  * @return {number[]}
  */
 var sortArrayByParity = function (nums) {
-  const resultArr = [];
+  const evenArr = nums.filter((num) => num % 2 === 0);
+  const oddArr = nums.filter((num) => num % 2 !== 0);
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] % 2 === 0) resultArr.push(nums[i]);
-  }
-
-  for (let j = 0; j < nums.length; j++) {
-    if (nums[j] % 2 !== 0) resultArr.push(nums[j]);
-  }
-  return resultArr;
+  return evenArr.concat(oddArr);
 };
