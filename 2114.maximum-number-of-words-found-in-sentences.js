8f7ecf702c624b46f5652e43254a2f9157be7e58
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-04-10 13:21:16 -0700

    solved Maximum Number of Words Found in Sentences

---
+++ 2114.maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/description/
+ */
+
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function (sentences) {
+  let countWord = 0;
+  for (let i = 0; i < sentences.length; i++) {
+    const sentence = sentences[i].split(/\s/g);
+    if (countWord < sentence.length) {
+      countWord = sentence.length;
+    }
+  }
+  return countWord;
+};
