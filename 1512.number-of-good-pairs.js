f2459d50c951711598e0b4ba4a0a7b371bd2d3df
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-04-16 16:58:27 -0700

    modified the number of equals sign

--- 1512.number-of-good-pairs.js
+++ 1512.number-of-good-pairs.js
@@ -10,7 +10,7 @@ var numIdenticalPairs = function (nums) {
   let count = 0;
   for (let i = 0; i < nums.length; i++) {
     nums.map((num, idx) => {
-      if (nums[i] == num && i != idx && i < idx) {
+      if (nums[i] === num && i !== idx && i < idx) {
         count++;
       }
     });
