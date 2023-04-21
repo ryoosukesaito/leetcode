7e57d7842c5bc10666298d63444ccd2c5a247746
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-04-21 00:40:10 -0700

    modified array to copyArray, which i used

--- 35.search-insert-position.js
+++ 35.search-insert-position.js
@@ -8,10 +8,11 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-  const findNum = nums.some((num) => num === target);
-  if (findNum) return nums.findIndex((num) => num === target);
+  const copyNumsArr = [...nums];
+  const findNum = copyNumsArr.some((num) => num === target);
+  if (findNum) return copyNumsArr.findIndex((num) => num === target);
 
-  nums.push(target);
-  nums.sort((a, b) => a - b);
-  return nums.findIndex((num) => num === target);
+  copyNumsArr.push(target);
+  copyNumsArr.sort((a, b) => a - b);
+  return copyNumsArr.findIndex((num) => num === target);
 };
