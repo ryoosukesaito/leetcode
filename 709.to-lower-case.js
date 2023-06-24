394048ff057dd64aaf45e874060b743619db7509
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-06-24 16:30:58 -0700

    To Lower Case

---
+++ 709.to-lower-case.js
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/to-lower-case/description/
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function (s) {
+  return s.toLowerCase();
+};
