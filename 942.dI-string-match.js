90b99a96e0e46ed765daf45821266b837f3830a5
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-06-30 22:57:10 -0700

    DI String Match

---
+++ 942.dI-string-match.js
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var diStringMatch = function (s) {
+  const arr = [];
+
+  for (let i = 0; i < s.length + 1; i++) {
+    arr.push(i);
+  }
+
+  let result = [];
+  let idx = 0;
+  while (arr.length > 0) {
+    let copyArr = [...arr];
+    if (s[idx] === "I") {
+      result.push(Math.min(...copyArr));
+      arr.shift();
+    } else {
+      result.push(Math.max(...copyArr));
+      arr.pop();
+    }
+    idx++;
+  }
+
+  return result;
+};
