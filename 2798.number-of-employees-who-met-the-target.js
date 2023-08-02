0038c7c24408b966e018b351ef017821b83b270d
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-08-02 12:05:28 -0700

    Number of Employees Who Met the Target

---
+++ 2798.number-of-employees-who-met-the-target.js
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode.com/problems/number-of-employees-who-met-the-target/
+ */
+
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function (hours, target) {
+  return hours.filter((hour) => hour >= target).length;
+};
